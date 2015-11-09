24a0db34445006e2ea2508b72fc801007358e905
John Slota <john.slota@gmail.com>
2015-11-09 12:02:14 -0700

    Set up integration testing and wrote user view test
    Added capybara to gemfile and test_helper. Added create_chip to
    test_helper. Wrote first integration test for user viewing products

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,24 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'capybara/rails'
+require 'mocha/mini_test'
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  def teardown
+    reset_session!
+  end
+
+  def create_chip(name, price, description)
+    Chip.create(name: name, price: price, description: description)
+  end
 end
