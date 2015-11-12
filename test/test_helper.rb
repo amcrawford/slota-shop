265486d2ef1ee8715f5fc8f51e95501ba4db586b
John Slota <john.slota@gmail.com>
2015-11-11 17:26:03 -0700

    Integrate simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'mocha/mini_test'
+require 'simplecov'
 
+SimpleCov.start("rails")
 
 class ActiveSupport::TestCase
 end
