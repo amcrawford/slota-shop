4bc534ce5defa407f4b9cd797fb5cb3d7857fd65
John Slota <john.slota@gmail.com>
2015-11-12 14:25:05 -0700

    Add Default Status to Order when created in test and software

--- test/integration/user_can_view_past_orders_test.rb
+++ test/integration/user_can_view_past_orders_test.rb
@@ -26,7 +26,7 @@ class UserCanViewPastOrdersTest < ActionDispatch::IntegrationTest
     category_1 = Oil.create(name: "Lard")
     item_1 = Chip.create(name: "Slotachips", price: 20, description: "Super yummy", oil_id: category_1.id)
     user = User.create(username: "John", password: "Password")
-    order = user.orders.create(total_price: 20)
+    order = user.orders.create(status: "Ordered", total_price: 20)
     order.chip_orders.create(chip_id: item_1.id, quantity: 1, subtotal: 20)
 
     visit '/'
@@ -41,6 +41,7 @@ class UserCanViewPastOrdersTest < ActionDispatch::IntegrationTest
     visit user_orders_path(user.id)
 
     click_link "View Order Details"
+    save_and_open_page
     within(".chips_ordered") do
       assert page.has_content?("Slotachips")
       assert page.has_content?("1")
@@ -49,6 +50,9 @@ class UserCanViewPastOrdersTest < ActionDispatch::IntegrationTest
       assert page.has_content?("available")
     end
 
+    within(".order_status") do
+      assert page.has_content?("Ordered")
+    end
     within(".order_total") do
       assert page.has_content?("20")
     end
