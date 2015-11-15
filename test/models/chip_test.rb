8fa0b332fe04c09571b35d2eebf7db00fda0a013
Amber Crawford <ammcrawford@gmail.com>
2015-11-15 11:23:31 -0700

    Add Chip Model Test

--- test/models/chip_test.rb
+++ test/models/chip_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class ChipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'a chip has many chip orders' do
+    create_user
+    create_shop
+    chip = Chip.all.first
+    order = Order.create(total_price: 20, user_id: User.all.first.id)
+    order.chip_orders.create(chip_id: chip.id, quantity: 1, subtotal: 20)
+    order.chip_orders.create(chip_id: chip.id, quantity: 2, subtotal: 40)
+
+    assert 2, chip.chip_orders.all.count
+  end
 end
