8a2a014f2df195935fb89c8c5b7a2a1bde0f7b5d
ShannonPaige <spaige@gmail.com>
2015-11-16 11:43:20 -0700

    update seed file to match paperclip image file paths

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Chip.create!([
     price: 6.99,
     description: "Whole fresh potatoes, sliced and cooked in lard, with salt added. ",
     oil_id: 1,
-    image_file_name: "http://www.getutz.com/media/k2/items/cache/f710044bf79a4b1f5d8b085e5e5d9711_M.jpg"},
+    image_file_name: "/utz_lardoriginal.jpg"},
   { name: "Jacksons Honest Potato Chips, Sweet Potato",
     price: 4.25,
     description: "Wade with coconut oil. Nature's most nourishing oil. Non GMO
@@ -22,20 +22,20 @@ Chip.create!([
     roots by using the finest potatoes available and kettle frying them by hand
     using organic coconut oil.",
     oil_id: 2,
-    image_file_name: "https://img.thrivemarket.com/store/566x/8/5/856823004110-1.jpg"},
+    image_file_name: "/jacksons_sweetpotato.jpeg"},
   { name: "Dang Toasted Coconut Chips",
     price: 10.79,
     description: "Our toasted coconut chips are a delicious snack with all the
     health benefits of coconut oil. They're so good, you'll say Dang! ",
     oil_id: 2,
-    image_file_name: "http://images.vitaminimages.com/pp/VF/Assets/Puritan_products/Detail_page/004049.jpg",
+    image_file_name: "/dang_coconutchips.jpeg",
     status: "retired"},
   { name: "Grandma Utz Handcooked Potato Chips BBQ",
     price: 6.99,
     description: "Slightly thicker, un-rinsed potato slices, kettle-cooked in
     lard in small batches at the best temperature for perfect crispness. Simply amazing!",
     oil_id: 1,
-    image_file_name: "http://www.getutz.com/media/k2/items/cache/0b1ad7a7b79268a1f4558db78e092446_M.jpg"},
+    image_file_name: "/utz_lardbbq.jpg"},
   ])
 
 
