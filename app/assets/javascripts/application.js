7d8c3dd6b48a5b5889d350abe0c73490fdb2a886
ShannonPaige <spaige@gmail.com>
2015-11-17 15:49:24 -0700

    Add ability to filter chips by name

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,20 @@ var loaded = function(){
   $(document).ready(function() {
     $('.modal-trigger').leanModal();
     $(".button-collapse").sideNav();
+
+    var $chips = $('.chip');
+
+    $('#name_search_name').keyup('change', function () {
+    var currentName = this.value;
+    $chips.each(function (index, chip) {
+      $chip = $(chip);
+      if ($chip.data('name').includes(currentName)) {
+        $chip.show();
+      } else {
+        $chip.hide();
+      }
+    });
+  });
   });
 }
 
