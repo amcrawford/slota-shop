b1dbedc0b5878e00f1f129966f4243cd3a89a12b
John Slota <john.slota@gmail.com>
2015-11-18 14:20:21 -0700

    hound changes

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,13 +7,14 @@ class NotificationsController < ApplicationController
   def self.notify(total)
     client = Twilio::REST::Client.new ENV["SID"], ENV["PIN"]
     message = client.messages.create from: ENV["PHONE"], to: ENV["WOW"],
-                                     body: 'Thanks for your order sucka, you"re total is ' "#{total}"
+                                     body: "Thanks for your order you're total is #{total}"
   end
 
   def self.call
     client = Twilio::REST::Client.new ENV["SID"], ENV["PIN"]
-    call = client.account.calls.create from: ENV["PHONE"],
-                                       to: ENV["WOW"], url: "http://demo.twilio.com/docs/classic.mp3"
+    client.account.calls.create from: ENV["PHONE"],
+                                to: ENV["WOW"],
+                                url: "http://demo.twilio.com/docs/classic.mp3"
   end
 
   def client
