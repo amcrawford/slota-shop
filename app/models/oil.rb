b26ffdb221c375083b5da2617304cefc3815315c
Amber Crawford <ammcrawford@gmail.com>
2015-11-18 15:56:17 -0700

    Add Oil Index IMages

--- app/models/oil.rb
+++ app/models/oil.rb
@@ -14,4 +14,14 @@ class Oil < ActiveRecord::Base
   def gimee_your_chips
     chips.available
   end
+
+  def oil_index_image
+    if name == "Avocado Oil"
+      "http://i.imgur.com/StaBSGO.jpg"
+    elsif name == "Coconut Oil"
+      "http://i.imgur.com/gn4vqHt.jpg"
+    else
+      "http://i.imgur.com/tvZ8urm.jpg"
+    end
+  end
 end
